358c4a7de1d95edc229dcd14bc27913d292c3ab2
David Rodriguez <darsco16@gmail.com>
2019-03-27 21:23:59 -0600

    Acerca de y Aviso de Privacidad

--- app/src/main/java/com/darsco/tjamich/acerca_de.java
+++ app/src/main/java/com/darsco/tjamich/acerca_de.java
@@ -1,13 +1,33 @@
 package com.darsco.tjamich;
 
+import android.content.Intent;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class acerca_de extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_acerca_de);
+
+        Button btn = (Button) findViewById(R.id.btnAviso);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), aviso_tjam.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/darsco/tjamich/aviso_tjam.java
@@ -0,0 +1,33 @@
+package com.darsco.tjamich;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class aviso_tjam extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aviso_tjam);
+
+        Button btn = (Button) findViewById(R.id.btnRegresar);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), acerca_de.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+        String str_text = "<a href=http://tjamich.gob.mx/Transparencia-2018-y-2015-2017 >Transparencia</a>";
+        TextView link;
+        link = (TextView) findViewById(R.id.txtAviso);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+        link.setText(Html.fromHtml(str_text));
+    }
+}
